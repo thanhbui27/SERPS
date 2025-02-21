d97d49e8b14f73ab973f17b9d9b41a95f029fe78
thanhbui27 <bthanh2001@gmail.com>
2025-02-21 18:08:31 +0700

    add vercel file config

--- app.js
+++ app.js
@@ -66,3 +66,6 @@ app.use((err, req, res, next) => {
 // Start the server
 const port = app.get("port") || 3000;
 app.listen(port, () => console.log(`Server is listening on port ${port}`));
+
+module.exports = app;
+
