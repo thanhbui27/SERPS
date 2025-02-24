b6bd58dfea9453c79199d17e0e798bd3e5f92970
thanhbui27 <bthanh2001@gmail.com>
2025-02-25 00:27:49 +0700

    fix swagger 1

--- app.js
+++ app.js
@@ -47,7 +47,14 @@ app.use(checkHeaders);
 // Routes
 app.use("/api/v1", apiRouters);
 
-app.use("/api/v1/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css"
+
+app.use("/api/v1/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec, {
+  customCss:
+      '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
+  customCssUrl: CSS_URL,
+}
+))
 
 
 // Routes
