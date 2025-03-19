8391fc694e88562c4fa38f81fe08068d4147c577
Bùi Ngọc Thành <63583108+thanhbui27@users.noreply.github.com>
2025-03-19 23:28:36 +0700

    add feature update and remove course (#9)

--- constants/roles.js
+++ constants/roles.js
@@ -1,8 +1,15 @@
 const Roles = Object.freeze({
-    ADMIN: "admin",
-    TEACHER: "teacher",
-    STUDENT: "student",
-    SUPERADMIN: "superadmin",
-  });
-  
-  module.exports = Roles;
+  ADMIN: "admin",
+  TEACHER: "teacher",
+  STUDENT: "student",
+  SUPERADMIN: "superadmin",
+});
+
+const roleLevels = {
+  student: 1,
+  teacher: 2,
+  admin: 3,
+  superadmin: 4,
+};
+
+module.exports = { Roles, roleLevels };
